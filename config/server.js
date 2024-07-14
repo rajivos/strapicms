be0beb451071ecf5c385b29501d466145d87eea7
rajivaux <raj.c.khushiram@gmail.com>
2024-07-15 09:11:50 +0930

    Keys being passed weird

--- config/server.js
+++ config/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 8080),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: "UUJVeh3kbVLXtytI2ZFeAQ==,HXYWyFrqj1cRaogmjKsoRw==,yhLbO9QbjIiqnUrUkRoorA==,FK/WmFCrj+QuJuNXSpY4iA==",
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
