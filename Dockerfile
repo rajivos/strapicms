1ccee9fa966da12c0d4440f6add00be090c33083
rajivaux <raj.c.khushiram@gmail.com>
2024-07-15 12:35:59 +0930

    cleaner .yml

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM node:18.17-alpine
 # Installing libvips-dev for sharp Compatibility
 RUN apk update && apk add --no-cache build-base gcc autoconf automake zlib-dev libpng-dev nasm bash vips-dev git
-ARG NODE_ENV=development
 ENV NODE_ENV=${NODE_ENV}
 
 WORKDIR /opt/
--- config/database.js
+++ config/database.js
@@ -1,17 +1,35 @@
- module.exports = ({ env }) => ({
+module.exports = ({ env }) => {
+  let dbConfig;
+
+  if (env("NODE_ENV") === "production") {
+    dbConfig = {
       connection: {
-        client: 'postgres',
+        client: "postgres",
         connection: {
-          host: env('DATABASE_HOST', 'localhost'),
-          port: env.int('DATABASE_PORT', 5432),
-          database: env('DATABASE_NAME', 'bank'),
-          user: env('DATABASE_USERNAME', 'postgres'),
-          password: env('DATABASE_PASSWORD', '0000'),
-          schema: env('DATABASE_SCHEMA', 'public'), // Not required
-          ssl: {
-            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
-          },
+          host: env("PROD_DATABASE_HOST", "localhost"),
+          port: env.int("PROD_DATABASE_PORT", 5432),
+          database: env("PROD_DATABASE_NAME", "prod_bank"),
+          user: env("PROD_DATABASE_USERNAME", "prod_postgres"),
+          password: env("PROD_DATABASE_PASSWORD", "prod_0000"),
         },
-        debug: false,
       },
-    });
+    };
+  } else {
+    dbConfig = {
+      connection: {
+        client: "postgres",
+        connection: {
+          host: env("DATABASE_HOST", "localhost"),
+          port: env.int("DATABASE_PORT", 5432),
+          database: env("DATABASE_NAME", "bank"),
+          user: env("DATABASE_USERNAME", "postgres"),
+          password: env("DATABASE_PASSWORD", "0000"),
+        },
+      },
+    };
+  }
+  return {
+    connection: dbConfig.connection,
+    debug: false,
+  };
+};
