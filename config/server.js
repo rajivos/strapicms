50062cc157b60bcb68b20b4f7aa9370aa7d2799c
rajivaux <raj.c.khushiram@gmail.com>
2024-07-15 11:11:52 +0930

    APP KEYS AS ARRAY

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 8080),
   app: {
-    keys: "UUJVeh3kbVLXtytI2ZFeAQ==,HXYWyFrqj1cRaogmjKsoRw==,yhLbO9QbjIiqnUrUkRoorA==,FK/WmFCrj+QuJuNXSpY4iA==",
+    keys: ["UUJVeh3kbVLXtytI2ZFeAQ==","HXYWyFrqj1cRaogmjKsoRw==","yhLbO9QbjIiqnUrUkRoorA==","FK/WmFCrj+QuJuNXSpY4iA=="],
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
