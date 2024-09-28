9b3c5d76c1e6ae83920fbc23c88b8a569caec2a3
Rajiv Khushiram <rajivkhushiram@hotmail.com>
2024-09-28 16:53:09 +0930

    replace service account role

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => {
     connection: {
       client: 'postgres',
       connection: {
-        host: env('DATABASE_HOST', 'localhost'), // dev keys
+        host: env('DATABASE_HOST', 'localhost'), // dev keyss
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'bank'),
         user: env('DATABASE_USERNAME', 'postgres'),
